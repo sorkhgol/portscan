22691ba2ff6ea435ae7a758e9f7d209d06a9100f
sorkhgol <42696790+sorkhgol@users.noreply.github.com>
2024-11-25 13:13:48 +0330

    Update portenum.py
    UDP scan

--- portenum.py
+++ portenum.py
@@ -3,8 +3,20 @@ import re
 from time import sleep
 from scapy.all import *
 
+def udpscan(dst_ip,dst_port):
+    pkt = sr1(IP(dst=dst_ip)/UDP(sport=5678,dport=dst_port),timeout = 2,verbose = 0)
+    if pkt == None:
+        print(dst_port, " UDP Open / filtered")
+    else:
+        if pkt.haslayer(ICMP):
+            print(dst_port, " UDP Closed")
+        elif pkt.haslayer(UDP):
+            print(dst_port, " UDP Open / filtered")
+        else:
+            print(dst_port, " UDP Unknown")
+
 def tcpscan(dst_ip,dst_port):
-    pkt = sr1(IP(dst=dst_ip)/TCP(sport=4567,dport=dst_port,flags="S"),timeout = 1,verbose = 0)
+    pkt = sr1(IP(dst=dst_ip)/TCP(sport=1234,dport=dst_port,flags="S"),timeout = 1,verbose = 0)
     if pkt != None:
         if pkt.haslayer(TCP):
             if pkt[TCP].flags == 20:
@@ -18,11 +30,12 @@ def tcpscan(dst_ip,dst_port):
         else:
             print(dst_port,"Unkown port")
     else:
-        print(dst_port,"Unanswered")
+        print(dst_port," TCP Unanswered")
+        udpscan(dst_ip=dst_ip,dst_port=dst_port)
 
 parser = argparse.ArgumentParser("Port Scanner")
-parser.add_argument("-i","--ip",help="Destination Ip for Scan",required=True);
-parser.add_argument("-p","--port",help="Destination Port for Scan",required=True);
+parser.add_argument("-i","--ip",help="Destination Ip for Scan",required=True)
+parser.add_argument("-p","--port",help="Destination Port for Scan",required=True)
 args = parser.parse_args()
 
 tempip = args.ip
